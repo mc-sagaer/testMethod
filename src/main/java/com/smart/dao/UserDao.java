caa514674a556c3690b659c19abef5443012ff63
ideaMc <932796837@qq.com>
2020-04-21 09:27:21 +0800

    2. 16

--- src/main/java/com/smart/dao/UserDao.java
+++ src/main/java/com/smart/dao/UserDao.java
@@ -13,7 +13,7 @@ import java.sql.SQLException;
 @Repository
 public class UserDao {
     private JdbcTemplate jdbcTemplate;
-    int aA;
+    int aA;  // AAA
 
     private  final static String MATCH_COUNT_SQL = " SELECT count(*) FROM t_user  " +
             " WHERE user_name =? and password=? ";
